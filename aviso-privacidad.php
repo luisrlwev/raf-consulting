decb6d9161d3ceb353d7e567717e31fc125653cc
Luis Angel Hernandez <rlwev@hotmail.com>
2025-08-11 17:34:06 -0500

    correccion del aviso de privacidad

--- aviso-privacidad.php
+++ aviso-privacidad.php
@@ -54,7 +54,7 @@
             <div class="row">
                 <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
                     <div class="section-title">
-                        <h2>Avios de privacidad</h2>
+                        <h2>AVISO DE PRIVACIDAD COMPLETO</h2>
                         <p class="text-left">
 RAFCO SERVICES, S.A. DE C.V., cuyo domicilio se encuentra en Blvd. Aldolfo Ruíz
 Cortínes No. 4302 Of. 110, Col. Jardines del Pedregal, Del. Coyoacán, C.P. 04500,
@@ -65,29 +65,42 @@ resguardarlos y manejarlos bajo completa confidencialidad, con las siguientes fi
 <br><br>
 a. Finalidad principal:
 <br><br>
-Sus datos personales, se tratarán con la finalidad principal de proveer los servicios de
-Farmacovigilancia y/o Tecnovigilancia cumpliendo con la Normatividad Oficial de
-Medicamentos y Dispositivos Médicos; en especial:
+Sus datos personales, se tratarán con la finalidad principal de proveer servicios
+Regulatorios, de Farmacovigilancia y/o Tecnovigilancia cumpliendo con la Normatividad
+Oficial de Medicamentos y Dispositivos Médicos; en especial:
 <br><br>
-- Atención de quejas: RAFCO SERVICES, S.A de C.V. podrá contactarle y dar
-seguimiento al desarrollo de proceso de quejas que usted inicie sobre alguno de
-los productos que manejamos de forma directa, o de nuestros clientes.
-<br><br>
-- Atención de eventos adversos (farmacovigilancia y/o tecnovigilancia): En caso de
-que se suscite algún evento que afecte a su salud, de algún producto de nuestros
-clientes y usted lo reporte con nosotros, se elaborará un registro con sus datos
-para atender, investigar y dar seguimiento al mismo.
-<br><br>
-- Investigación sobre la existencia y seguimiento a eventos adversos de productos
-en estudio, ya sea por medio de pacientes y/o profesionales de la salud.
+<ul>
+    <li>Datos personales que se requieren para solicitudes ante COFEPRIS o para dar de alta al cliente en nuestro sistema de facturación, asegurándonos que cubren los requisitos esperados.</li>
+    <li>Atención de quejas: RAFCO SERVICES, S.A de C.V. podrá contactarle y dar seguimiento al desarrollo de proceso de quejas que usted inicie sobre alguno de los productos que manejamos de forma directa, o de nuestros clientes.</li>
+    <li>Atención de eventos adversos (farmacovigilancia y/o tecnovigilancia): En caso de que se suscite algún evento que afecte a su salud, de algún producto de nuestros clientes y usted lo reporte con nosotros, se elaborará un registro con sus datos para atender, investigar y dar seguimiento al mismo.</li>
+    <li>Investigación sobre la existencia y seguimiento a eventos adversos de productos en estudio, ya sea por medio de pacientes y/o profesionales de la salud.</li>
+</ul>
 <br><br>
 b. Finalidades secundarias.
 <br><br>
-Prestación de algún otro servicio solicitado por usted, para lo cual nos
-proporcione sus datos personales.
+Prestación de algún otro servicio solicitado por usted, para lo cual nos proporcione sus
+datos personales.
 <br><br>
 Sus datos personales no serán utilizados para fines de mercadotecnia, publicidad o
 prospección comercial ni para ninguna otra finalidad.
+<br><br>
+RAFCO SERVICES, S.A de C.V. se compromete a lo siguiente:
+<br><br>
+    <ul>
+        <li>En todo momento observar los principios de licitud, finalidad, lealtad,
+        consentimiento, calidad, proporcionalidad, información y responsabilidad en el
+        tratamiento de sus datos personales y se obliga a nunca obtener y tratar datos
+        personales a través de medios engañosos.</li>
+        <li>Establecer y mantener medidas de seguridad administrativas, técnicas y físicas
+        que permitan proteger sus datos personales contra daño, pérdida, alteración,
+        destrucción o el uso, acceso o tratamiento no autorizado.</li>
+        <li>Procurará que sus datos personales contenidos en sus bases de datos sean
+        exactos, completos, correctos y actualizados.</li>
+        <li>Informará de manera inmediata sobre cualquier vulneración de seguridad que
+        ocurra en cualquier etapa del tratamiento de sus datos personales y que afecte de
+        forma significativa sus derechos patrimoniales o morales, a fin de que esta pueda
+        tomar las medidas necesarias para la protección y defensa de sus derechos.</li>
+    </ul>
 <br><br>
 <strong>2. Datos personales a los que se dará tratamiento.</strong>
 <br><br>
@@ -103,10 +116,14 @@ a. Datos no sensibles
 b. Datos sensibles
 <br><br>
 Antes de solicitar o dar tratamiento a sus datos sensibles, deberemos conforme al
-Artículo 9 de la Ley Federal de Protección de Datos Personales en Posesión de los
-Particulares, deberá darnos su consentimiento expreso ya sea oral o por escrito para su
-tratamiento, a través de su consentimiento oral, firma autógrafa, firma electrónica, o
-cualquier mecanismo de autenticación que al efecto se establezca.
+<i>Artículo 8 de la Ley Federal de Protección de Datos Personales en Posesión de
+los Particulares, el cual establece que: deberá darnos su consentimiento expreso
+ya sea oral o por escrito para su tratamiento, a través de su consentimiento oral,
+firma autógrafa, firma electrónica, o cualquier mecanismo de autenticación que al
+efecto se establezca.
+De igual forma, no podrán crearse bases de datos que contengan datos
+personales sensibles, sin que se justifique la creación de las mismas para
+finalidades legítimas, concretas y acordes con las actividades o fines explícitos.</i>
 <br><br>
 - Evento o Incidente adverso<br>
 - Historia clínica u otros datos relacionados con su salud
@@ -120,30 +137,41 @@ limitado a la prestación de los servicios aquí descritos.
 <br><br>
 <strong>3. Remisión y transmisión de sus datos personales</strong>
 <br><br>
-Sus datos personales podrán ser remitidos a terceros que nos auxilian en la prestación de nuestros
-servicios y que la Ley de la materia denomina encargados, los que deberán dar cumplimiento a
-este aviso de privacidad.
+Sus datos personales podrán ser remitidos a terceros que nos auxilian en la prestación de
+nuestros servicios y que la Ley de la materia denomina encargados, los que deberán dar
+cumplimiento a este aviso de privacidad.
 Asimismo serán transmitidos a nuestros clientes (responsable del producto en cuestión) y
-a las autoridades pertinentes, manteniendo su identidad en el anonimato, con el objeto de
-dar cumplimiento de las NOM-220-SSA1 y NOM-240-SSA1 en las versiones más
-actualizadas respectivamente.
-En caso de que sus datos vayan a a ser transferidos a un tercero, obtendremos su
-consentimiento previo, salvo el caso en el que por disposición legal podamos o estemos
-obligados a transmitir sus datos.
+a las autoridades pertinentes, en su caso, se mantiene la identidad de los pacientes en el
+anonimato, con el objeto de dar cumplimiento de las NOM-220-SSA1 y NOM-240-SSA1
+en las versiones más actualizadas respectivamente.
+En caso de que sus datos vayan a ser transferidos a un tercero, obtendremos su
+consentimiento previo, salvo el caso en el que por disposición legal o la naturaleza de los
+servicios contratados, se espere o estemos obligados a transmitir sus datos.
 <br><br>
 <strong>4. Ejercicio de Derechos ARCO</strong>
 <br><br>
-Usted tiene derecho de acceder, rectificar e incluso solicitar el retiro de sus datos
-personales, así como de oponerse al tratamiento de los mismos o revocar el
-consentimiento que para tal fin nos haya otorgado, todo esto, a través de los
-procedimientos que se han implementado. Para conocer dichos procedimientos,
-información que se solicita y tiempos de respuesta, se puede poner en contacto con el
-personal encargado de los datos personales de RAFCO SERVICES, S.A. DE C.V., cuyo
-domicilio se encuentra en Blvd. Aldolfo Ruíz Cortínes No. 4302 Of. 110, Col. Jardines
-del Pedregal, Del. Coyoacán, C.P. 04500, Cuidad de México., al número (55) 5683-
-5496 con las personas responsables de las Unidades de Tecnovigilancia y
-Farmacovigilancia, o al correo farmacovigilancia@rafconsulting.net o visitar nuestra
-página de Internet <a href="https://www.rafconsulting.net">www.rafconsulting.net</a>
+Usted tiene derecho en todo momento, solicitar el acceso, rectificación, cancelación u
+oposición (derechos ARCO) respecto al tratamiento de sus datos personales. En
+particular, la cancelación implica que la persona titular tiene derecho a solicitar que sus
+datos personales sean eliminados de los archivos, registros, expedientes y sistemas del
+responsable, a fin de que los mismos ya no se encuentren en su posesión.
+Para conocer dichos procedimientos, información que se solicita y tiempos de respuesta,
+se puede poner en contacto con el personal encargado de los datos personales de
+RAFCO SERVICES, S.A. DE C.V., cuyo domicilio se encuentra en Blvd. Aldolfo Ruíz
+Cortínes No. 4302 Of. 110, Col. Jardines del Pedregal, Del. Coyoacán, C.P. 04500,
+Cuidad de México., al número (55) 5683-5496 con las personas responsables de las
+Unidades de Tecnovigilancia y Farmacovigilancia, o al correo
+farmacovigilancia@rafconsulting.net o visitar nuestra página de Internet
+www.rafconsulting.net
+Es importante mencionar que la solicitud para el ejercicio de los derechos ARCO deberá
+contener y acompañar lo siguiente:
+<ol>
+    <li>El nombre de la persona titular y su domicilio o cualquier otro medio para recibir notificaciones;</li>
+    <li>Los documentos que acrediten la identidad de la persona titular o, en su caso, la personalidad e identidad de su representante;</li>
+    <li>La descripción clara y precisa de los datos personales respecto de los que se busca ejercer alguno de los derechos ARCO, salvo que se trate del derecho de acceso;</li>
+    <li>La descripción del derecho ARCO que se pretende ejercer, o bien, lo que solicita la persona titular, y</li>
+    <li>Cualquier otro elemento o documento que facilite la localización de los datos personales</li>
+</ol>
 <br><br>
 <strong>5. Opciones y medios para limitar el uso de divulgación de los datos personales:</strong>
 <br><br>
@@ -167,7 +195,7 @@ se hará público a través de nuestra página web. En caso de que por virtud de
 modificación o actualización, se requiera de su consentimiento expreso, lo contactaremos
 directamente
 <br><br>
-Fecha última de actualización Octubre de 2020</p>
+Fecha última de actualización Junio de 2025</p>
                     </div>
                 </div>
             </div>
